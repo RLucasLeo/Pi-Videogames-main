b33933dce578736c5cde2305478b88335fa47922
lucas <robledolucas42@gmail.com>
2023-04-26 11:39:04 -0300

    Conexion db muchos a muchos

--- api/src/db.js
+++ api/src/db.js
@@ -6,6 +6,7 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
+//AQUI SE HACE LA CONCEXION A LA BASEDEDATOS
 const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
@@ -30,10 +31,12 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { Videogame } = sequelize.models;
+const { Videogame, Genres } = sequelize.models;
 
-// Aca vendrian las relaciones
+// Aca vendrian las relaciones DE **MUCHOS A MUCHOS**
 // Product.hasMany(Reviews);
+Videogame.belongsToMany(Genres, {trough: 'Videogame_Genre'});
+Genres.belongsToMany(Videogame, {trough: 'Videogame_Genre'});
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
