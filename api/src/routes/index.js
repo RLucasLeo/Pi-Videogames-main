95a4953b72716c88273a646d0a4927c46cf64544
lucas <robledolucas42@gmail.com>
2023-04-26 15:54:16 -0300

    borrado ruta videogameS

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,6 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-//const videogamesRoute = require("./videogames")
 //const genresRoute = require ("./genres.js")
 //const videogameRoute = require ("./videogame.js")
 
