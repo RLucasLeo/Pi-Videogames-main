7c2692e83a86b01a5bdc3978ddc5cd48015aae78
lucas <robledolucas42@gmail.com>
2023-04-26 12:34:38 -0300

    configurada las routers, comentadas

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
+//const videogamesRoute = require("./videogames")
+//const genresRoute = require ("./genres.js")
+//const videogameRoute = require ("./videogame.js")
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+//router.use('/videogames', videogamesRoute);
+//router.use('/genres', genresRoute);
+//router.use('/videogame', videogameRoute);
 
 module.exports = router;
