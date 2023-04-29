33b1e8f909956f13af476131885808d345a463f5
lucas <robledolucas42@gmail.com>
2023-04-29 11:31:19 -0300

    ruta, archivo Genres hechos, modificado models de Genres de uuid a integer

--- api/src/models/Genres.js
+++ api/src/models/Genres.js
@@ -3,7 +3,7 @@ const {DataTypes} = require('sequelize');
 module.exports = (sequelize) => {
     sequelize.define('genres', {
         id:{
-            type: DataTypes.UUID,
+            type: DataTypes.INTEGER,
             defaultValue: DataTypes.UUIDV4,
             unique: true,
             primaryKey: true,
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,7 +1,7 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-//const genresRoute = require ("./genres.js")
+const genresRouter = require ("./genres.js")
 const videogameRouter = require ("./videogame.js")
 
 const router = Router();
@@ -9,7 +9,6 @@ const router = Router();
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 router.use('/videogames', videogameRouter);
-//router.use('/genres', genresRoute);
-//router.use('/videogame', videogameRoute);
+router.use('/genres', genresRouter);
 
 module.exports = router;
