80380d8f1dfa066869aeeb823b537decc19b8bfa
lucas <robledolucas42@gmail.com>
2023-04-29 19:00:53 -0300

    Creacion y estilos del landingpage

--- client/src/Components/landingPage/landing.jsx
+++ client/src/Components/landingPage/landing.jsx
@@ -1,15 +1,16 @@
 import styles from "./landing.module.css";
 import {Link} from "react-router-dom"
+import React from "react";
 
-export const landingPage = () => {
+const LandingPage =()=> {
 
-  
     return (
           <div className={styles.containerCentral}>
-            <div className={styles.containerSubtitulo}>
-              <h3>Bienvenido a mi pagina!!</h3>
-            </div>
-            <div className={styles.containerButton}><Link to="/home"><button >Ingresar!</button></Link></div>
+              <div className={styles.containerSubtitulo}>
+                  <h3>Bienvenido a mi pagina!!</h3>
+              </div>
+            <Link to="/home"><button >Ingresar!</button></Link>
           </div>
     )
   }
+  export default LandingPage;
