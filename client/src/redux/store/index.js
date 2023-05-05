43cefebde1e1017d682b86b3940361c09f461242
lucas <robledolucas42@gmail.com>
2023-05-05 13:25:05 -0300

    store creado y exportado

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,8 +1,11 @@
 import {applyMiddleware, createStore} from 'redux';
 import {composeWithDevTools} from "redux-devtools-extension";
 import thunk from "redux-thunk";
+import rootReducer from '../reducer';
 
-export const store = createStore(
+const store = createStore(
     rootReducer,
-    composeWithDevTools(applyMiddleware)
-)
+    composeWithDevTools(applyMiddleware(thunk))
+  );
+  
+  export default store;
