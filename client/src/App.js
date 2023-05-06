9eab45759e664ea1039caceeed9ee897415b2994
lucas <robledolucas42@gmail.com>
2023-05-06 00:02:27 -0300

    componente erro404 creado

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import LandingPage from "./Components/landingPage/landing"
 import Home from "./Components/homePage/home"
 import Detail from "./Components/Detail/Detail"
 import Create from "./Components/Create/Create"
-
+import Error404 from "./Components/Error404/Error404"
 import { Route, Routes } from "react-router-dom";
 import './App.css';
 
@@ -14,6 +14,7 @@ function App() {
         <Route path="/home" element={<Home/>}/>
         <Route path="/detail/:id" element={<Detail/>}/>
         <Route path="/create" element={<Create/>}/>
+        <Route path={'*'} element={<Error404/>}/>
       </Routes>
     </div>
   );
