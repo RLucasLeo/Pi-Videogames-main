e9a0e9b3328d471ba712f94d99a34c6a8e7fedba
lucas <robledolucas42@gmail.com>
2023-05-06 09:48:11 -0300

    image renderizada front, arreglado

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -9,6 +9,7 @@ class Card extends React.Component {
            <div className={s.card}>
                
                    <h3 className={s.nombre}>{this.props.name}</h3>
+                   <img className={s.image} src={this.props.image} alt=""/>
                    <p className={s.genres}>{this.props.genres}</p>
                    <p className={s.rating}>⭐ {this.props.rating}</p>
                 <Link to={`/detail/${this.props.id}`} className={s.navLink}><span className={s.leer_mas}>Leer más</span></Link>
