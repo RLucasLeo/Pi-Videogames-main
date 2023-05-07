f235e5f720763185af7b4312f6be394513f3e096
lucas <robledolucas42@gmail.com>
2023-05-07 19:38:14 -0300

    filtro genres arreglado reducer

--- client/src/redux/reducer/index.js
+++ client/src/redux/reducer/index.js
@@ -99,7 +99,7 @@ function rootReducer(state=initialState, action){
              case  FILTER_BY_GENRES:
                 let auxi=[];
                 if(action.payload){
-                    auxi = state.genres.filter(e=>{
+                    auxi = state.videogames.filter(e=>{
                         if(e.genres.length === 0){
                             return e.genres;
                         }
