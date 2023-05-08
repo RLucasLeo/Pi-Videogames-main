fefd5607ab87fe2789740b16acfc6ed2043b3c15
lucas <robledolucas42@gmail.com>
2023-05-07 22:55:34 -0300

    modificado navbar y getName para buscar por barra

--- api/src/Controllers/getName.js
+++ api/src/Controllers/getName.js
@@ -3,54 +3,19 @@ require('dotenv').config()
 const {API_KEY} = process.env
 const {Videogame} = require("../db")
 const { Op } = require("sequelize")
+const {getGames} = require("./getGames")
 
 const getName =async (name) => {
-    try {
-         const apiGames = await axios.get(`https://api.rawg.io/api/games?search=${name}&key=${API_KEY}`)
-    const apiGamesList = apiGames.data.results.map((game)=> ({
-            id: game.id,
-            name: game.name,
-            genres: game.genres?.map((gen)=> gen.name),
-            platforms: game.platforms? game.platforms.map((plat)=> plat.platform.name).join(', '):null,
-            released: game.released,
-            image: game.image,
-            rating: game.rating,
-    }));
-//traemos juegos desde la base de datos
-    const dbGames = await Videogame.findAll({
-        where : {
-            name: {
-                [Op.iLike]: `%${name}%`,},
-            },
-        include: {
-            model: Genre,
-            as: 'Genres',
-            attributes: ["name"],
-            through: {
-                attributes: [],
-            },
-        },
-        
-    });
-    const dbGameList = Array.isArray(dbGames)? dbGames.map((game)=>({
-        
-            id: game.id,
-            name: game.name,
-            genres: game.genres?.map((gen)=> gen.name),
-            platforms: game.platforms?game.platforms.map((plat)=> plat.platform.name).split(',').join(', '): null,
-            released: game.released,
-            image: game.image,
-            rating: game.rating,
-        
-    })):[];
-    const games= apiGamesList.concat(dbGameList);
-    if(games.length=== 0){
-        return null;
+    const juegos = await getGames();
+    if(name){
+        const filtrado = juegos.filter((game)=> game.name.toLowerCase().includes(name.toLowerCase())).splice(0, 15);
+    
+    if(filtrado.length>0){
+        return filtrado;
+    } else {
+        throw new Error('No se encontro el juego con ese nombre')
     }
-    return games.slice(0, 15);
-    } catch (error) {
-        console.error(error);
-        throw new Error("ERROR AL BUSCAR JUEGOS")
-    }
+} else {return videogame}
 }
+
 module.exports = {getName}
--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,10 +1,40 @@
 import styles from "./Navbar.module.css"
+import React, {useState} from "react";
+import {useDispatch} from "react-redux"
+import {getNames} from "../../redux/actions";
+
+
 const Navbar =() =>{
+
+  const [state, setState]=useState('');
+  const dispatch= useDispatch();
+
+  function handleChange(e){
+    e.preventDefault()
+    setState(e.target.value)
+  }
+
+  function handleSubmit(e){
+    e.preventDefault();
+    if(state.length>1){
+      dispatch(getNames(state))
+      setState('')
+    }else{
+      alert('Ingrese un nombre para buscar')
+    }
+  }
+
+
    return(
     <div className={styles.navbar}>
-      <form >
-        <input placeholder="Buscar por nombre" className={styles.searchbar}/>
-        <button className={styles.Button}>Buscar</button>
+      <form onSubmit={e=> handleSubmit(e)}>
+        <input type="text"
+        id="name"
+        autoComplete="off"
+        value={state}
+         placeholder="Buscar juego"
+         onChange={e=> handleChange(e)} className={styles.searchbar}/>
+        <button type="submit" className={styles.Button}>Buscar</button>
       </form>
     </div>
     
